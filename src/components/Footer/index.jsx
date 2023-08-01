0f102d42423b05ec883588545cb7f3d4176e0457
JulieBourelDrapier <juliebourel@hotmail.com>
2023-08-01 12:12:13 +0200

    Add adjustments to footer.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,25 @@
 import logo from '../../assets/logoFooter.png'
 import styled from 'styled-components';
 
+const FooterWrapper = styled.footer`
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+  width: 100%;
+  background-color: #000;
+  margin: 0;
+`;
+
+const FooterImage = styled.img`
+ object-fit: cover;
+ text-align: center;	
+`;
+
 function Footer() {
   return (
-      <footer>
-          <img src={logo} alt="logo du site Kasa"/>
-      </footer>
+      <FooterWrapper>
+          <FooterImage src={logo} alt="logo du site Kasa"/>
+      </FooterWrapper>
   );
 }
 
