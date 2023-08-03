bc2ed0a311764d3607e0e9f2f8497f09a106423e
JulieBourelDrapier <juliebourel@hotmail.com>
2023-08-03 17:06:50 +0200

    add modifications Banner (overlay) => yet to be modified

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import imageBanner from '../../assets/imagebanner.png';
 
-const BannerWrapper = styled.figure`
+const FigureContainer = styled.div`
+`
+const Figure = styled.figure`
 width: 100%;
 margin: 0;
 margin-top: 27px;
@@ -29,38 +31,58 @@ const BannerImage = styled.img`
   object-fit: cover;
   width: 100%;
   border-radius: 10px;
-
 `;
 
 const BannerText = styled.figcaption`
-  background-color: #000;
-  opacity: 0.5;
+  // background-color: #000;
+  opacity: 1;
   color: #fff;
-  text-align: center;
+  font-size: 24px;
+  font-family: Montserrat;
+  font-weight: 500;
+  width: 100%;
   border-radius: 10px;
   position: absolute;
-  font-size: 24px;
-  font-weight: 500;
-  font-family: Montserrat;
-  width: 100%;
+
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  font-weight: bold;
+  padding-left: 16px;
+  //line-height: 1.5;
+
+  
 
   @media (max-width: 768px) {
     padding-top: 30px;
-    padding-bottom: 52px;
+    padding-bottom: 45px;
   }
 
   @media (min-width: 768px) {
     padding-top: 111px;
     padding-bottom: 82px;
+    text-align: center;
   }
 `;
 
+const BannerOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  `
+
 function Banner() {
   return (
-    <BannerWrapper>
-      <BannerImage src={imageBanner} alt="Paysage de fond" />
+    <FigureContainer>
+      <Figure>
+        <BannerImage src={imageBanner} alt="Paysage de fond" />
         <BannerText>Chez vous, partout et ailleurs.</BannerText>
-    </BannerWrapper>
+      </Figure>
+      <BannerOverlay />
+    </FigureContainer>
   );
 }
 
