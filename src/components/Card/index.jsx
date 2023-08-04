550574a850d119d1db7d3f77bf2708c5816ca0c2
JulieBourelDrapier <juliebourel@hotmail.com>
2023-08-04 12:18:10 +0200

    Add modification of Semantic HTML elements of Card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,20 +2,21 @@ import { PropTypes } from 'prop-types'
 import styled from 'styled-components'
 
 
-const CardContainer = styled('section')`
+const CardSection = styled('section')`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  width: 100%; 
 
   @media (min-width: 768px) {
       background-color: rgba(246, 246, 246, 1);
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
+      flex-direction: row; 
+      justify-content: space-around;
       flex-wrap: wrap;
-      width: 90%;
+    }
 `
 
-const CardWrapper = styled('card')`
+const CardDiv = styled('div')`
   margin-top: 27px;
   position: relative;
   margin-bottom: 20px;
@@ -24,41 +25,49 @@ const CardWrapper = styled('card')`
   }
 `
 
-const CardImage = styled('img')`
-width: auto;
-height: 255px;
-border-radius: 10px;
-object-fit: cover;
+const CardCover = styled('img')`
+  width: 100%;
+  border-radius: 10px;
+  object-fit: cover;
+  max-height: 255px;
+
+  @media (min-width: 768px) {
+    max-height: 340px;
+  }
 `
 
-const CardLabel = styled('figcaption')`
-font-family: Montserrat;
-color: #fff;
-font-size: 18px;
-font-weight: 500;
-position: absolute;
-border-radius: 10px;
-top: 210px;
-text-align: left;	
-width: 96%;
-padding-left: 15px;
-background-color: rgba(0, 0, 0, 0.4);
+const CardTitle = styled('h3')`
+// font-family: Montserrat;
+// color: #fff;
+// font-size: 18px;
+// font-weight: 500;
+// position: absolute;
+// border-radius: 10px;
+// top: 210px;
+// text-align: left;	
+// width: 96%;
+// padding-left: 15px;
+// background-color: rgba(0, 0, 0, 0.4);
 
-@media (min-width: 768px) {
-  max-width: 300px;
-}
+// @media (min-width: 768px) {
+//   max-width: 300px;
+// }
 ` 
+const CardLabel = styled('p')`
+  color : #fff;
+  font-size: 44px;
+`
 
 
 function Card({id, label, title, cover}) {
   return (
-    <CardContainer>
-      <CardWrapper>
-        <CardLabel>{label}</CardLabel>
-        <CardImage src={cover} alt=" photo du logement en question" />
-          <figcaption>{title}</figcaption>
-      </CardWrapper>
-    </CardContainer>
+    <CardSection>
+        <CardDiv>
+            <CardCover src={cover} alt=" photo du logement en question" />
+            <CardTitle>{title}</CardTitle>  
+            <CardLabel>{label}</CardLabel>
+        </CardDiv>
+    </CardSection>
 )
 }
 
