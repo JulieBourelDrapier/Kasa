439565b997a53a2e381017f24924b86a5e15a2e2
JulieBourelDrapier <juliebourel@hotmail.com>
2023-08-10 01:27:28 +0200

    Add media queries to indicate changes of left and right arrows of gallery according to size screen (gallery.jsx)

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { useState } from 'react'
 import LeftArrow from '../assets/left-arrow.png';	
 import RightArrow from '../assets/right-arrow.png';
+import SmallLeftArrow from '../assets/smallleftarrow.png';
+import SmallRightArrow from '../assets/smallrightarrow.png';
 
 const FigureContainer = styled.div`
 `
@@ -26,11 +28,19 @@ const LeftArrowSlide = styled.img`
 position: absolute;
 left: 0;
 margin: 15px;
+
+@media (min-width: 768px) {
+  content: url(${LeftArrow});
+}
 `
 const RightArrowSlide = styled.img`
 position: absolute;
 right: 0;
 margin: 15px;
+
+@media (min-width: 768px) {
+  content: url(${RightArrow});
+}
 `
 
 function Gallery({imgSrc}) {
@@ -38,8 +48,8 @@ function Gallery({imgSrc}) {
 
   return (
     <FigureContainer>
-        <LeftArrowSlide src={LeftArrow} onClick={() => setIndexGallery(indexGallery - 1 >= 0 ? indexGallery - 1 : imgSrc.length - 1 )}/>
-        <RightArrowSlide src={RightArrow} onClick={() => setIndexGallery(indexGallery + 1 < imgSrc.length ? indexGallery + 1 : 0 )}/>
+        <LeftArrowSlide src={SmallLeftArrow} onClick={() => setIndexGallery(indexGallery - 1 >= 0 ? indexGallery - 1 : imgSrc.length - 1 )}/>
+        <RightArrowSlide src={SmallRightArrow} onClick={() => setIndexGallery(indexGallery + 1 < imgSrc.length ? indexGallery + 1 : 0 )}/>
         <Slides src={imgSrc[indexGallery]} alt="Photos du logement sélectionné" />
         <p>{indexGallery + 1}/{imgSrc.length}</p>
     </FigureContainer>
