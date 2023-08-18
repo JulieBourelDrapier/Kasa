7dd68d96e2159d16810f88b537bab75ab40f5c9c
JulieBourelDrapier <juliebourel@hotmail.com>
2023-08-18 12:48:36 +0200

    Try ErrorPage in case of incorrect ID

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.css'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './pages/home'
 import About from './pages/about'
 import Header from './components/header'
