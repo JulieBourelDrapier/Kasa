df4e86d5756993a2658ed0bc5840c14f52d1690f
JulieBourelDrapier <juliebourel@hotmail.com>
2023-08-21 12:42:21 +0200

    Add linear gradient on Card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -13,22 +13,39 @@ const CardCover = styled('img')`
 width: 100%;
 border-radius: 10px;
 height: 255px;	
+position: relative;
 
 @media (min-width: 500px) {
   height: 340px;
 }
 `
+const CardDiv = styled('div')`
+position: relative;
+display: inline-block;
+
+&::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 99%;
+  background: linear-gradient(10deg, rgba(0, 0, 0, 1) 0%, rgba(255, 255, 255, 0.5) 50%, #ff6060 100%);
+  opacity: 0.6;
+  border-radius: 10px;
+}
+`
+
 const CardFigcaption = styled('figcaption')`
 color: #fff;
 font-size: 15px;
 position: absolute;
 bottom: 4px;
 border-radius: 0 0 10px 10px;
-width: 100%;
-padding: 9px;	
-background-color: rgba(0, 0, 0, 0.5);
+width: 70%;
+padding: 9px;
 box-sizing: border-box;
-min-height: 45px;
+// height: 100%;
 
 @media (min-width: 768px) {
   padding: 4px;
@@ -43,8 +60,10 @@ min-height: 45px;
 function Card({id, label, title, cover}) {
   return (
          <CardFigure>
-          <CardCover src={cover} alt=" photo du logement en question" />
-          <CardFigcaption>{label}</CardFigcaption>
+          <CardDiv>
+            <CardCover src={cover} alt=" photo du logement en question" />
+          </CardDiv>
+            <CardFigcaption>{label}</CardFigcaption>
         </CardFigure> 
 )
 }
